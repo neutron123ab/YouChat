e87617ed4ad22cc5644c4605b4f0261e0dc3e2d5
neutron123ab <neutron123ab@163.com>
2022-11-06 20:15:42 +0800

    chat entity

---
+++ src/main/java/com/neutron/youchat_backend/entity/GroupChat.java
@@ -0,0 +1,35 @@
+package com.neutron.youchat_backend.entity;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.stereotype.Repository;
+
+//群聊
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Repository
+public class GroupChat {
+
+    //用户id
+    private Integer userId;
+    //用户名
+    private String username;
+
+    //群聊id
+    private Integer groupId;
+    //群聊名
+    private String groupName;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    //发送时间
+    private String sendTime;
+
+    //消息内容
+    private String content;
+
+}
