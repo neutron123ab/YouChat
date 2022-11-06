623b3e11b0f6ae232086dbb12a08ed9d6e05760b
neutron123ab <neutron123ab@163.com>
2022-11-06 14:08:29 +0800

    first commit

--- src/main/java/com/neutron/youchat_backend/config/SecurityConfig.java
+++ src/main/java/com/neutron/youchat_backend/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.neutron.youchat_backend.config;
 
+import com.neutron.youchat_backend.filter.LoginFilter;
 import com.neutron.youchat_backend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -16,6 +17,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -26,6 +28,9 @@ public class SecurityConfig {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private LoginFilter loginFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -39,6 +44,7 @@ public class SecurityConfig {
                 .cors()
                 .configurationSource(corsConfigurationSource())
                 .and()
+                .addFilterBefore(loginFilter, UsernamePasswordAuthenticationFilter.class)
                 .csrf().disable()
                 .build();
     }
