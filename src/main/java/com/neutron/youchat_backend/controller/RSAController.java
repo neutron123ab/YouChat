1c7683c79a822dbc3d358dc93386c8e161ba5047
neutron123ab <neutron123ab@163.com>
2022-11-09 13:17:53 +0800

    first commit

--- src/main/java/com/neutron/youchat_backend/controller/RSAController.java
+++ src/main/java/com/neutron/youchat_backend/controller/RSAController.java
@@ -1,6 +1,7 @@
 package com.neutron.youchat_backend.controller;
 
 import com.neutron.youchat_backend.common.RSAUtils;
+import com.neutron.youchat_backend.common.Result;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,9 +13,9 @@ public class RSAController {
      * @return rsa公钥
      */
     @GetMapping("/getPublicKey")
-    public String getPublicKey(){
+    public Result<String> getPublicKey(){
         RSAUtils rsaUtils = RSAUtils.getRsaUtils();
-        return rsaUtils.getPublicKey();
+        return Result.success(rsaUtils.getPublicKey());
     }
 
 }
